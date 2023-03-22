86788196733d16c3d743bda6ddb603d16f7d16a9
0b1kn00b <polysemantic@gmail.com>
2023-03-22 10:05:40 +0100

    separate test folder

--- Main.hx
+++ Main.hx
@@ -3,10 +3,6 @@ using stx.Log;
 
 class Main{
   static function main(){
-    final logger = __.log().global;
-          logger.includes.push("stx/coroutine");
-          logger.includes.push("stx/test");
-          logger.includes.push('haxe/overrides');
-    stx.coroutine.Test.main();
+    
   }
 }
