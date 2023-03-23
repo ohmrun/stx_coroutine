7b67b5381b8cea0730264f42e894923e0d48fd27
0b1kn00b <polysemantic@gmail.com>
2023-03-23 07:30:00 +0100

    import fletcher Core

---
+++ src/main/haxe/stx/coroutine/import.hx
@@ -0,0 +1,18 @@
+package stx.coroutine;
+
+using eu.ohmrun.fletcher.Core;
+using eu.ohmrun.Fletcher;
+using stx.Coroutine;
+using stx.coroutine.Core;
+using tink.CoreApi;
+
+using stx.Pico;
+using stx.Fail;
+using stx.Nano;
+using stx.Fn;
+using stx.Log;
+using stx.Pkg;
+using stx.Stream;
+using stx.coroutine.Logging;
+
+using eu.ohmrun.Fletcher;
